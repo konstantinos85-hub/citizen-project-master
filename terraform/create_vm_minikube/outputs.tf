21590471687b496671de6cdeab4fb68ead84f528
konstantinos85-hub <konstantinoskouyouris@yahoo.com>
2026-02-05 16:33:49 +0100

    Final working version with deb package

--- terraform/create_vm_minikube/outputs.tf
+++ terraform/create_vm_minikube/outputs.tf
@@ -1,11 +1,10 @@
-# 1. IP διεύθυνση του στιγμιοτύπου
 output "instance_public_ip" {
-  description = "Η δημόσια IP διεύθυνση του στιγμιοτύπου εικονικής μηχανής που δημιουργήθηκε"
-  value       = aws_instance.minikube_server.public_ip
+  description = "Η δημόσια IP της εικονικής μηχανής"
+  value       = aws_instance.minikube.public_ip
 }
 
-# 2. Αναγνωριστικό του στιγμιοτύπου
 output "instance_id" {
-  description = "Το μοναδικό αναγνωριστικό (ID) του στιγμιοτύπου εικονικής μηχανής στο AWS"
-  value       = aws_instance.minikube_server.id
+  description = "Το ID της εικονικής μηχανής"
+  value       = aws_instance.minikube.id
 }
+
